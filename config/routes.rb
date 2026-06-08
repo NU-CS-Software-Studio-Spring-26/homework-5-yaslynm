93d828aa2c2d2f0453d2962478fcd149edb971a8
yaslynm <152467060+yaslynm@users.noreply.github.com>
2026-06-08 17:26:55 -0400

    Add priority marker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :todos
+  resources :todos do
+    patch :toggle_priority, on: :member
+  end
   get '/hello', to: 'todos#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
